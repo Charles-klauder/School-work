f9b052303d945f1b54a845403886905e2921e57d
Charles <94377179+Charles-klauder@users.noreply.github.com>
2022-05-10 10:11:11 -0400

    Update DesktopProject.py

--- DesktopProject.py
+++ DesktopProject.py
@@ -1,3 +1,15 @@
-def function(input):
-    return input
-print(function("Hello World"))
+def recurseforever(x):
+    def recurse(x)
+        print(x)
+        x = x + 1
+    print(x)
+    x = x + 1
+    recurseforever(x)
+#recurseforever(0)
+desk1 = []
+desk2 = []
+Storage = "C:\Users\XLHS\OneDrive\Desktop\altdesk"
+temp = "C:\Users\XLHS\OneDrive\Desktop\Temp"
+import shutil
+#shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
+shutil.move("C:\Users\XLHS\OneDrive\Desktop\altdesk\test.txt", "C:\Users\XLHS\OneDrive\Desktop\Temp")
